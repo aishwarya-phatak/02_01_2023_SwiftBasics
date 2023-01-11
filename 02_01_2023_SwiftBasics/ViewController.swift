193a4ed02098e35d4c3ee8ef55dbf9ba9faa0c80
aishwarya <ashp17star@gmail.com>
2023-01-11 14:35:56 +0530

    Collections In Swift - Arrays, Set, Dictionary, forEach, for in, Optionals, Optional Binding

--- 02_01_2023_SwiftBasics/ViewController.swift
+++ 02_01_2023_SwiftBasics/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         basics()
+        basics2()
     }
     
     func basics(){
@@ -116,5 +117,108 @@ class ViewController: UIViewController {
             print("Key that is a part of dictionary -- \(keyPresent)")
         }
         checkValue(key: "rollNo")
+    }
+    
+    
+    func basics2(){
+        //var arrays
+        var numbers : [Int] = [10,10,12,13,14,15]
+        var names : [String] = ["Pooja","Pratiksha","Sejal","Tushar"]
+        var percentages : [Double] = [13.23,35.23,45.56,67.45]
+
+        for eachNumber in numbers{
+            print(eachNumber + 10)
+        }
+
+        for eachName in names{
+            print("Name is : \(eachName)")
+        }
+
+        for eachPercentage in percentages{
+            print(eachPercentage)
+        }
+
+        var marks = [60.67,56.45,78.56,10.45,46.56]
+
+        //way 1 --> for-in loop
+        var sum : Double = 0
+        for mark in marks {
+            sum += mark
+        }
+
+        print("The sum of student marks is : \(sum)")
+
+        //way 2 --> forEach loop iteration
+        var sum1 : Double = 0
+        marks.forEach { mark in
+            sum1 += mark
+        }
+
+        print("\nSum -- \(sum1)")
+
+        var countOfElements = marks.count
+        print(countOfElements)
+
+        var percentageOfStudent = Int(sum) / countOfElements
+
+        print(percentageOfStudent)
+        var student : [String : Any] = [
+                       "firstName" : "Tushar",
+                       "lastName" : "Patil",
+                       "rollNumber" : 12
+        ]
+
+
+        for (key, value) in student{
+            print("\(key) -- \(value)")
+        }
+
+        //Array
+        var numberArray : [Int] = [10,10,12,23,132,34]
+        var arrays1 : [Any] = [10,12,"Tejas",34.54]
+        for value in arrays1{
+            print(value)
+        }
+
+        arrays1.forEach { value in
+            print(value)
+        }
+
+        //task --> convert the string n --> bITcODE
+        //var n = "BitCode"
+        //var num6 = 1234     //calculate the addition of 1+2+3+4 --> 10
+        //task 3 -> array of numbers --> 34, 56, 34, 23, 56  --> sum of elements, even or odd, no is prime or not?, array reverse, minimum & maxmium elemnts from an array.
+
+        //ranges in swift
+        for i in 1...10{
+            print(i * 2)
+        }
+
+
+        for i in 1..<11{
+            print(i * 5)
+        }
+
+        print(arrays1[1...3])
+
+        var num7 = 1
+        switch(num7){
+            case 1 :
+                print("Case 1 executed")
+            case 2 : print("Case 2 exexuted")
+            case 80...100 :
+                print("range from 80 to 100 executed")
+            default:
+                print("Default case")
+        }
+
+        //Set
+        var setN : Set = ["Tushar", "Mayuri","Sachin","Pooja","Pooja"]
+        var setM : Set = [1,2,3,4,1,2]
+
+        setM.forEach {
+            print($0)
+        }
+
     }
 }
